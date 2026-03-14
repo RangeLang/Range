cef0440dba77c85a726492e57cac66d48a4e6b40
George Tchelidze <bulbs.carrots_0a@icloud.com>
2026-03-14 23:44:39 +0400

    Keep Neat keywords highlighted under parse errors

--- zed/neat/languages/neat/highlights.scm
+++ zed/neat/languages/neat/highlights.scm
@@ -18,17 +18,19 @@
 ] @keyword
 
 ; Control flow
-(if_statement "if" @keyword.control)
-(if_statement "else" @keyword.control)
-(for_statement "for" @keyword.control)
-(for_statement "in" @keyword.control)
-(while_statement "while" @keyword.control)
-(switch_statement "switch" @keyword.control)
-(switch_case "case" @keyword.control)
-(switch_default "default" @keyword.control)
-(return_statement "return" @keyword.control)
-(break_statement) @keyword.control
-(continue_statement) @keyword.control
+[
+  "if"
+  "else"
+  "for"
+  "in"
+  "while"
+  "switch"
+  "case"
+  "default"
+  "return"
+  "break"
+  "continue"
+] @keyword.control
 
 ; Projection keyword
 "on" @keyword
