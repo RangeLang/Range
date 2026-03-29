f1fa78729b94879d2a137cdbe34cada1abba0750
George Tchelidze <bulbs.carrots_0a@icloud.com>
2026-03-29 22:17:39 +0400

    Refine core literal initializers

--- NeatPlayground/.neat/Build/swift/Sources/Playground.swift
+++ NeatPlayground/.neat/Build/swift/Sources/Playground.swift
@@ -3,13 +3,12 @@ import Foundation
 @main
 struct NeatMain {
     static func main() throws {
-        let numbers = [1, 2, 3, 4]
-        var total = 0
-        Logger.info("Neat playground")
-        for number in numbers {
-            total += number
-            Logger.log(total)
-        }
-        Logger.success("sum is \(total == 10 ? "correct" : "incorrect")")
+        var person = Person(name: "George", age: 26)
+        person.age += 1
+        let personString: String = {
+                "Person: \(person.name), Age: \(person.age)"
+        }()
+        var user = User(person: person)
+        user.incrementAge()
     }
 }
